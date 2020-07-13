3abc86ed255f32077791596441d07ebd80433c45
PLM <850003188@qq.com>
2020-07-13 13:16:41 +0800

    Mybatis 增删改查CURD + 实体类属性名与数据库中表的字段名不一致的解决办法

--- src/main/java/com/plm/dao/IUserDao.java
+++ src/main/java/com/plm/dao/IUserDao.java
@@ -19,4 +19,41 @@ public interface IUserDao {
      *  查询所有
      */
     List<User> findAll();
+
+    /**
+     * 保存用户
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户信息　
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据 id 删除用户信息
+     * @param userId
+     */
+    void deleteUserById(Integer userId);
+
+    /**
+     * 根据 id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 查询总用户数目
+     * @return
+     */
+    int findTotal();
 }
--- src/test/java/MyBatisTest.java
+++ src/test/java/MyBatisTest.java
@@ -4,8 +4,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 
@@ -28,23 +33,128 @@ import java.util.List;
  */
 public class MyBatisTest {
 
-    public static void main(String[] args) throws Exception {
+    private InputStream in;
+    private SqlSession session;
+    private IUserDao userDao;
+
+    /**
+     * 初始化
+     * @throws Exception
+     * @Before 定义此方法在测试方法之前执行
+     */
+    @Before
+    public void init() throws Exception {
         // 1、读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+         in = Resources.getResourceAsStream("SqlMapConfig.xml");
         // 2、创建 SqlSessionFactory工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
         // 3、使用工厂生产 SqlSession对象
-        SqlSession session = factory.openSession();
+         session = factory.openSession();
         // 4、使用 SqlSession创建 Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
+         userDao = session.getMapper(IUserDao.class);
+    }
+
+    /**
+     *  释放资源
+     * @throws Exception
+     * @After 定义此方法在测试方法之后执行
+     */
+    @After
+    public void destory() throws Exception{
+
+        // 6、提交事务  默认关闭自动提交事务功能
+        session.commit();
+        // 7、释放资源
+        session.close();
+        in.close();
+    }
+
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public  void testFindAll() {
+
         // 5、使用代理对象执行方法
         List<User> users = userDao.findAll();
         for(User user : users){
             System.out.println(user);
         }
-        // 6、 释放资源
-        session.close();
-        in.close();
+    }
+
+    /**
+     * 保存用户信息
+     */
+    @Test
+    public void testSaveUser() {
+        // 要保存的用户信息
+        User user = new User();
+        user.setUsername("testSaveUser and get id");
+        user.setAddress("武汉");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+        System.out.println("保存操作之前 ："+user);
+        // 5、使用代理对象执行方法
+        userDao.saveUser(user);
+        // 保存用户信息后，获取插入数据的 id
+        System.out.println("保存操作之后 ："+user);
+    }
+
+    /**
+     * 更新用户信息
+     */
+    @Test
+    public void testUpdateUser(){
+        User user = new User();
+        user.setId(53);
+        user.setUsername("testUpdateUser");
+        user.setAddress("武汉");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        // 5、使用代理对象执行方法
+        userDao.updateUser(user);
+    }
+
+    /**
+     *  根据 id 删除用户信息
+     */
+    @Test
+    public void testDeleteUserById(){
+        // 5、使用代理对象执行方法
+        userDao.deleteUserById(53);
+    }
+
+    /**
+     * 根据 id 查询用户
+     */
+    @Test
+    public void testFindById(){
+        // 5、使用代理对象执行方法
+        User user = userDao.findById(51);
+        System.out.println(user);
+    }
+
+    /**
+     * 根据名称 模糊查询用户信息
+     */
+    @Test
+    public void testFindByName(){
+        // 5、使用代理对象执行方法
+        List<User> lists = userDao.findByName("%王%");
+        for(User user : lists){
+            System.out.println(user);
+        }
+    }
+
+    /**
+     * 查询总用户数目
+     */
+    @Test
+    public void testFindTotal(){
+        // 5、使用代理对象执行方法
+        int total = userDao.findTotal();
+        System.out.println("total = "+total);
     }
 }
